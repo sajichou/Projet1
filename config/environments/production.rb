e213b08147f6c0371c4d92ed560d4de44057df9d
sajid <sajid123@hotmail.fr>
2018-04-26 14:41:11 +0200

    Mailer1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   }
 
   #config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
-  config.action_mailer.default_url_options = { host: 'lakamaraderie.herokuapp.com.'}
+  #config.action_mailer.default_url_options = { host: 'lakamaraderie.herokuapp.com.'}
 
   config.assets.initialize_on_precompile = false
 
