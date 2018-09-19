e2f268571d32740ad4af5b4e49f1a356188a82ed
sajid <sajid123@hotmail.fr>
2018-09-19 11:00:28 +0200

    mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   #default from: 'contact@lakamaraderie.fr'
-  default from: "TopNote <contact@topnote.fr>"
+  default from: "contact@topnote.fr"
   #add_template_helper(EmailHelper)
   layout 'mailer'
 end
